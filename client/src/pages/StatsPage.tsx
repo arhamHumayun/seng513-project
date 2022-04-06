bff83b81f5f01cac6df148dc71b4b56bf687528e
Aluminiumiodide <41275949+Aluminiumiodide@users.noreply.github.com>
2022-04-06 07:08:21 -0600

    a

--- client/src/pages/StatsPage.tsx
+++ client/src/pages/StatsPage.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { useState } from 'react';
 import NavBar from '../components/NavBarFull';
 import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import chart1 from './../assets/chart1.png';
+import chart2 from './../assets/chart2.png';
+import chart3 from './../assets/chart3.png';
+import chart4 from './../assets/chart4.png';
 
 const center = {
   position: 'absolute' as 'absolute',
@@ -37,61 +41,51 @@ const line = {
 	background: 'black'
 }
 
+const titleSize = {
+  fontSize : '28px'
+}
+
+const chartSize = {
+  height : '180px',
+  width : '288px',
+}
+
 function Stats() {
   return (
 	  <div>
       <NavBar/>       
       <Container style={center}>
         <Row className="mt-4 px-5" style={border}>
-					<Row className="text-right"><Col>
-						<Button variant="outline-dark" href="/settings">Typing Statistics</Button>
+          <Row className="text-left"><Col>
+						<Button variant="outline-dark" href="/profile">Back</Button>
 					</Col></Row>
-					<Row className="mt-4 mb-2">
-            <Form.Label className="text-left">Statistics For Today:</Form.Label>
-          </Row>
+					<Row className="mb-4"><Col>
+						<Form.Label variant="outline-dark" href="/settings" style={titleSize}>Typing Statistics</Form.Label>
+					</Col></Row>
+
 					<Row>
-						<Col style={border2} className="ml-4">
-							<Form.Label className="mt-2">Total Races</Form.Label>
-							<Col><Form.Label>4</Form.Label></Col>
+						<Col className="ml-5">
+              <Row className="mb-3" style={chartSize}><img src={chart1}/></Row>
+              <Row><Form.Label>Key Frequency</Form.Label></Row>
 						</Col>
 						<Col></Col>
-						<Col style={border2}>
-							<Form.Label className="mt-2">Average Speed (wpm)</Form.Label>
-							<Col><Form.Label>64.3</Form.Label></Col>
-						</Col>
-						<Col></Col>
-						<Col style={border2} className="mr-4">
-							<Form.Label className="mt-2">Top Speed (wpm)</Form.Label>
-							<Col><Form.Label>71.6</Form.Label></Col>
+						<Col className="mr-5">
+              <Row className="mb-3" style={chartSize}><img src={chart2}/></Row>
+              <Row><Form.Label>Relative Typing Speed</Form.Label></Row>
 						</Col>
 					</Row>
-					<Row className="mt-5 mb-3">
-            <div style={line}/>
-          </Row>
 
 					<Row className="mt-4 mb-2">
-            <Form.Label className="text-left">All-Time Statistics:</Form.Label>
-          </Row>
-					<Row className="mb-3">
-						<Col style={border2} className="ml-4">
-							<Form.Label className="mt-2">Total Races</Form.Label>
-							<Col><Form.Label>56</Form.Label></Col>
+						<Col className="ml-5">
+              <Row className="mb-3" style={chartSize}><img src={chart3}/></Row>
+              <Row><Form.Label>Practice Calendar</Form.Label></Row>
 						</Col>
 						<Col></Col>
-						<Col style={border2}>
-							<Form.Label className="mt-2">Average Speed (wpm)</Form.Label>
-							<Col><Form.Label>68.5</Form.Label></Col>
-						</Col>
-						<Col></Col>
-						<Col style={border2} className="mr-4">
-							<Form.Label className="mt-2">Top Speed (wpm)</Form.Label>
-							<Col><Form.Label>74.6</Form.Label></Col>
+						<Col className="mr-5">
+              <Row className="mb-3" style={chartSize}><img src={chart4}/></Row>
+              <Row><Form.Label>Typing Speed</Form.Label></Row>
 						</Col>
 					</Row>
-					
-					<Col className="mt-4">
-						<Button variant="outline-dark" href="/stats">View Stats</Button>
-					</Col>
 
 				</Row>
       </Container>
